f957ce9103f29c314e7c4a3e5c187ad28a7c2800
Armine <am.khachatryan1989@gmail.com>
2018-03-28 20:45:38 -0700

    envirement change to production

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'superPassword' => '111112',
+    'definition'         => [
+
+            'db.host' => '127.0.0.1',
+            'db.name' => 'sfltask',
+            'db.user' => 'root',
+            'db.pass' => '',
+
+
+    ],
+    "apiUrl"     => "http://sfltask.com/api/",
+    'environment' => 'production',
+
+
+
+
+
+];
